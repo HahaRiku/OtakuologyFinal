f2cf49e88dba9f66b136f7d6c4faad25ab5b65a4
HahaRiku <s9323839@gmail.com>
2019-06-15 18:02:32 +0900

    First commit

---
+++ Assets/Scripts/DanmuDesign.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*****************************
+ * 
+ * 進行一次華麗的彈幕(???
+ * 
+ * *****************************/
+
+
+public class DanmuDesign : MonoBehaviour {
+
+    public enum Type {
+        圓形散開,
+        螺旋
+    }
+
+    public Type type = Type.圓形散開;
+    public Sprite 彈幕的圖;
+    public GameObject Danmu_element;
+    public float elementSize= 50;
+    public float elementSpeed = 1;
+
+    private bool start;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (start && !GlobalVariables.gameOver && !GlobalVariables.win) {
+            start = false;
+            if (type == Type.圓形散開) {
+                for (int i = 0; i < 10; i++) {
+                    EachNodeMovingControl obj = Instantiate(Danmu_element, gameObject.transform).GetComponent<EachNodeMovingControl>();
+                    obj.Initialize(elementSize, 36 * i, elementSpeed, 彈幕的圖);
+                    GlobalVariables.AddDanmuElement(obj.gameObject);
+                    obj.Run();
+                }
+            }
+            else if (type == Type.螺旋) {
+                for (int i = 0; i < 10; i++) {
+                    EachNodeMovingControl obj = Instantiate(Danmu_element, gameObject.transform).GetComponent<EachNodeMovingControl>();
+                    obj.Initialize(elementSize, 36 * i, elementSpeed, 彈幕的圖);
+                    GlobalVariables.AddDanmuElement(obj.gameObject);
+                    StartCoroutine(WaitAndRun(i * 0.2f, obj));
+                }
+            }
+        }
+    }
+
+    public void StartDanmu() {
+        start = true;
+    }
+
+    private IEnumerator WaitAndRun(float seconds, EachNodeMovingControl obj) {
+        yield return new WaitForSeconds(seconds);
+        obj.Run();
+    }
+}
---
+++ Assets/Scripts/EachNodeMovingControl.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EachNodeMovingControl : MonoBehaviour {
+
+    private float size_pixel = 50;  //square
+    private float direction = 0;    //0~359
+    private float speed = 1;
+
+    private bool start = false;
+    private RectTransform objRT;
+    private Image img;
+    private RectTransform parentRT;
+
+    void Start() {
+
+        
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (start) {
+            objRT.localPosition = new Vector3(
+                objRT.localPosition.x + speed * Mathf.Cos(direction * Mathf.PI / 180),
+                objRT.localPosition.y + speed * Mathf.Sin(direction * Mathf.PI / 180),
+                objRT.localPosition.z);
+            if (Mathf.Abs(objRT.localPosition.x + parentRT.localPosition.x) >= GlobalVariables.gameWinX / 2 - size_pixel/2 ||
+                Mathf.Abs(objRT.localPosition.y + parentRT.localPosition.y) >= GlobalVariables.gameWinY / 2 - size_pixel/2) {
+                //print(gameObject.name);
+                //print(GlobalVariables.gameWinX / 2 - size_pixel);
+                //print(objRT.localPosition.x);
+                Destroy(gameObject);
+            }
+        }
+	}
+
+    public void Initialize(float s_p, float d, float s, Sprite sprite) {
+        objRT = gameObject.GetComponent<RectTransform>();
+        objRT.sizeDelta = new Vector2(size_pixel, size_pixel);
+        img = gameObject.GetComponent<Image>();
+        parentRT = gameObject.transform.parent.gameObject.GetComponent<RectTransform>();
+        size_pixel = s_p;
+        if (objRT == null) {
+            print("not");
+        }
+        else {
+            objRT.sizeDelta = new Vector2(size_pixel, size_pixel);
+        }
+        direction = d;
+        speed = s;
+        img.sprite = sprite;
+    }
+
+    public void Run() {
+        start = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D coll) {
+        if (coll.name == "Player") {
+            GlobalVariables.playerHP -= 1;
+            Destroy(gameObject);
+        }
+    }
+}
+
